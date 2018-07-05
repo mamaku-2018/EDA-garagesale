f2849bdbe651f4e3126dca245120e4241dbe6892
Rachael Peri <raheraperi@Rachaels-MacBook-Air.local>
2018-07-06 08:13:44 +1200

    test data complete

---
+++ data/items.js
@@ -0,0 +1,18 @@
+module.exports = {
+  items: [
+    {
+      id: 1,
+      name: 'Mike Mee Goreng',
+      description: 'A fun snack'
+    }, {
+      id: 1,
+      name: 'Don Teacher',
+      description: 'Our sensei'
+
+    }, {
+      id: 1,
+      name: 'Deep Dive Dougs',
+      description: 'Wedding speech writer extrodinaire'
+    }
+  ]
+}
