fcedfcffee6d0ec574f005ddfa1929a84b8c383a
bronburgundy <bron.cotter@icloud.com>
2018-07-06 10:53:04 +1200

    Added images and associated descriptions in seeds. Deleted DBto run again

--- migrations/20180706095115_items.js
+++ migrations/20180706095115_items.js
@@ -1,6 +1,6 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTableIfNotExists('Items', function (table) {
-    table.increments().primary()
+    table.increments('id').primary()
     table.string('name')
     table.string('description')
     table.integer('price')
