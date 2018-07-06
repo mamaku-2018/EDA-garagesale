8d0a55858098529176434aaef907bf1498564f40
Matthew McGregor <Mattjackmcgregor@gmail.com>
2018-07-06 12:36:17 +1200

    connected reducers actions to main component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import Nav from './Nav'
-import Banner from './Banner'
+// import Nav from './Nav'
+// import Banner from './Banner'
 import Main from './Main'
-import Cart from './Cart'
+// import Cart from './Cart'
 
 
 class App extends React.Component {
   render () {
     return (
       <div className='app-container'>
-        <Nav/>
-        <Banner/>
+        {/* <Nav/> */}
+        {/* <Banner/> */}
         <Main/>
-        <Cart/>
+        {/* <Cart/> */}
       </div>
     )
   }
--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,4 +1,38 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
+import {receiveItems, fetchItems} from '../actions'
+import Item from './Item'
+class Main extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
 
-export default Main
+  componentDidMount () {
+    this.props.dispatch(receiveItems())
+  }
+  render () {
+    return (
+      <div className='main'>
+        <ul>
+          {this.props.items.map(item => {
+            return (
+              <Item key={item.id} item={item}/>
+            )
+          })
+          }
+        </ul>
+      </div>
+
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    items: state.items
+  }
+}
+
+export default connect(mapStateToProps)(Main)
