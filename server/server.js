8d414479c71250ea121d7fda48f3785c63d81e37
Rachael Peri <rachael.peri@gmail.com>
2018-07-09 14:21:06 +1200

    adding passport

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const express = require('express')
+const passport = require('passport')
 const routes = require('./routes/routes')
 const auth = require('./routes/auth')
 
 const server = express()
 
 server.use(express.json())
+server.use(passport.initialize())
 server.use(express.static(path.join(__dirname, './public')))
 
 server.use('/api/v1/items', routes)
